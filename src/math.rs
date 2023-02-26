5e4952eae3823e44078cc19c772b77405b38829d
Johan Solbakken <johsol@stud.ntnu.no>
2023-02-26 22:13:43 +0100

    Random functions

--- src/math.rs
+++ src/math.rs
@@ -1,5 +1,12 @@
+use rand::Rng;
+
 
 
 pub fn random_double() -> f64 {
-    0.0
+    let mut rng = rand::thread_rng();
+    rng.gen()
+}
+
+pub fn random_double_between(min:f64, max:f64) -> f64 {
+    min + (max - min) * random_double()
 }
